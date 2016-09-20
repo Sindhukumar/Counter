2660c5de87d8bc176dbc13a064a5d799defb2ec9
Sindhukumar <ssundar6@gmu.edu>
2016-09-20 10:31:11 -0400

    Interupt Added

--- src/Mom.java
+++ src/Mom.java
@@ -2,20 +2,20 @@
 public class Mom extends Thread {
 
 	Thread last;
-	public Mom(Thread last){
-		this.last = last; 
+
+	public Mom(Thread last) {
+		this.last = last;
 	}
-	public void run(){
-		
-//		for(int i=0;i<1000;i++)
-		while(last.isAlive()){
-		System.out.println("-------------------------Mom-------------------------");
-		try {
-			this.sleep(2000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+
+	public void run() {
+
+		while (last.isAlive()) {
+			System.out.println("-------------------------Mom-------------------------");
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 }
